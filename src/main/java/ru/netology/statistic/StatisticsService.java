7fdaa1a202625a6249fd195a1e3ccd79da1a1328
jukkty <dima.iachimov@gmail.com>
2021-03-31 11:58:22 +0300

    back to first moves

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -10,14 +10,9 @@ public class StatisticsService {
   public long findMax(long[] incomes) {
     long current_max_index = 0;
     long current_max = incomes[0];
-    long counter = 0;
-    for (long income : incomes) {
-      counter++;
-      if (current_max <= income) {
-        current_max_index = counter;
+    for (long income : incomes)
+      if (current_max < income)
         current_max = income;
-      }
-    }
-    return current_max_index;
+    return current_max;
   }
 }
